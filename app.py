39f154e7692b47edcabc2627cc5e140823082a3d
FelixHerrera21 <felixshadow21@gmail.com>
2023-06-06 17:51:31 -0600

    puerto

--- app.py
+++ app.py
@@ -54,5 +54,5 @@ def transform_text():
             return jsonify(response)
 
 if __name__ == '__main__':
-    #app.run(host='0.0.0.0', port=8080)
-    app.run(port=5000)
+    app.run(host='0.0.0.0', port=5000)
+    #app.run(port=5000)
